7f198360bb92293f9d7b0aedec20e54e19cb968d
Asis <asiso7@gmail.com>
2022-09-03 16:06:02 +0200

    Working Docker container

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import openai
 import os
 from fastapi import FastAPI
-from functions import *
 
 # read API key from system environment
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -21,6 +20,19 @@ def get_suggestions(query: str) -> dict:
     """
     Get categories from a free text query
     """  
-    response = get_categories(query)
+    restart_sequence = "\n"
+
+    response = openai.Completion.create(
+    model="text-davinci-002",
+    prompt=f"The following is a list of queries and the categories they fall into.\n\nExample of possible categories:\n\"Cleaning\", \"Hotel & catering supplies\", \"Shipping, packaging\", \"Building services engineering\", \"Medical, therapy, laboratory supplies\", \"Industrial\", \"Occupational safety\", \"Office & warehouse equipment\", \"Electronics\", \"Professional Tools\", \"Computers & accessories\", \"Stationery\"\n\nquery: I need a cleaning trolley\ncategories: Cleaning, Household, Industrial\n\nquery: I need office tables\ncategories: Office & warehouse equipment, Furniture\n\nquery: I need ink for the printer\ncategories: Computers & accessories, Stationary\n\nquery: I need cooking utensils\ncategories: Hotel & catering supplies, Kitchen\n\nquery: I need a new computer\ncategories: Computers & accessories, Electronics\n\nquery: {text_query}\ncategories:",
+    temperature=0.19,
+    max_tokens=31,
+    top_p=1,
+    best_of=3,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=["\n"]
+    )
+    response = [response['choices'][0]['text']]
 
     return {'suggestions': response}
