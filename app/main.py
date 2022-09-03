d485642a70ee7d5c4c5ff647e70d5e474b67ed2c
Asis <asiso7@gmail.com>
2022-09-03 16:55:30 +0200

    Update Readme

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def read_root():
 
 
 @app.get("/suggestions/", status_code=200)
-def get_suggestions(query: str) -> dict:
+def get_suggestions(input_query: str) -> dict:
     """
     Get categories from a free text query
     """  
@@ -24,7 +24,7 @@ def get_suggestions(query: str) -> dict:
 
     response = openai.Completion.create(
     model="text-davinci-002",
-    prompt=f"The following is a list of queries and the categories they fall into.\n\nExample of possible categories:\n\"Cleaning\", \"Hotel & catering supplies\", \"Shipping, packaging\", \"Building services engineering\", \"Medical, therapy, laboratory supplies\", \"Industrial\", \"Occupational safety\", \"Office & warehouse equipment\", \"Electronics\", \"Professional Tools\", \"Computers & accessories\", \"Stationery\"\n\nquery: I need a cleaning trolley\ncategories: Cleaning, Household, Industrial\n\nquery: I need office tables\ncategories: Office & warehouse equipment, Furniture\n\nquery: I need ink for the printer\ncategories: Computers & accessories, Stationary\n\nquery: I need cooking utensils\ncategories: Hotel & catering supplies, Kitchen\n\nquery: I need a new computer\ncategories: Computers & accessories, Electronics\n\nquery: {text_query}\ncategories:",
+    prompt=f"The following is a list of queries and the categories they fall into.\n\nExample of possible categories:\n\"Cleaning\", \"Hotel & catering supplies\", \"Shipping, packaging\", \"Building services engineering\", \"Medical, therapy, laboratory supplies\", \"Industrial\", \"Occupational safety\", \"Office & warehouse equipment\", \"Electronics\", \"Professional Tools\", \"Computers & accessories\", \"Stationery\"\n\nquery: I need a cleaning trolley\ncategories: Cleaning, Household, Industrial\n\nquery: I need office tables\ncategories: Office & warehouse equipment, Furniture\n\nquery: I need ink for the printer\ncategories: Computers & accessories, Stationary\n\nquery: I need cooking utensils\ncategories: Hotel & catering supplies, Kitchen\n\nquery: I need a new computer\ncategories: Computers & accessories, Electronics\n\nquery: {input_query}\ncategories:",
     temperature=0.19,
     max_tokens=31,
     top_p=1,
